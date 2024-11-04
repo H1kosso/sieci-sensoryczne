a1546d4014aaccdcceedb6d75815619519c53228
Marcin Onik <115038725+H1kosso@users.noreply.github.com>
2024-11-04 18:06:30 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -4,33 +4,24 @@ const wss = new WebSocket.Server({ port: 8080 });
 
 console.log('Server listening on port 8080');
 
-const externalWsUrl = 'ws://localhost:9090';
+const clients = new Set();
 
 wss.on('connection', (ws) => {
     console.log('New ws connection');
+    clients.add(ws);
 
     ws.on('message', (message) => {
         console.log('New data:', message);
-
-        const externalWs = new WebSocket(externalWsUrl);
-
-        externalWs.on('open', () => {
-            externalWs.send(message);
-            console.log(`Forwarded data to external WebSocket: ${message}`);
-        });
-
-        externalWs.on('message', (externalMessage) => {
-            console.log(`Received from external server: ${externalMessage}`);
-            ws.send(`Response from external server: ${externalMessage}`);
-        });
-
-        externalWs.on('close', () => {
-            console.log('Connection to external WebSocket closed');
+        clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(`${message}`);
+            }
         });
     });
 
     ws.on('close', () => {
         console.log('WS connection closed');
+        clients.delete(ws);
     });
 
     ws.send('Hello.');
